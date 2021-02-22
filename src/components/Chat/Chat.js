46a29b91b7c0c348935de7abf6240b8831145b0e
Clément François <clement.francois.developpeur@outlook.com>
2021-02-22 18:15:57 +0100

    Create the Chat
    Create the Chat component let this component use 75% of the screen and the entire view height

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ChatHeader from '../ChatHeader/ChatHeader';
+import './Chat.css';
+
+const Chat = () => {
+    return (
+        <div className='chat'>
+            <ChatHeader />
+
+            
+        </div>
+    )
+}
+
+export default Chat;
