e38dd9a9efd01c699bb97605f4451a30a54d5194
Clément François <clement.francois.developpeur@outlook.com>
2021-02-22 14:26:13 +0100

    Sidebar component
    Create Sidebar component inside src/components/Sidebar folder

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidebar = () => {
+    return (
+        <div className='sidebar'>
+            <h2>The Sidebar component</h2>
+        </div>
+    )
+}
+
+export default Sidebar
