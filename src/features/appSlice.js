21294e9b6f0adc9a76ec27f94a99989929d823a0
Clément François <clement.francois.developpeur@outlook.com>
2021-02-23 21:22:06 +0100

    Update appSlice.js
    Create the setChannelInfo function inside the appReducer

--- src/features/appSlice.js
+++ src/features/appSlice.js
@@ -10,13 +10,14 @@ const appSlice = createSlice({
     },
 
     reducers: {
-        setChannelId: (state, action) => {
-
+        setChannelInfo: (state, action) => {
+            state.channelId = action.payload.channelId;
+            state.channelName = action.payload.channelName;
         }
     }
 });
 
-export const { setChannelId } = appSlice.actions;
+export const { setChannelInfo } = appSlice.actions;
 
 export const selectChannelId = (state) => state.app.channelId;
 export const selectChannelName = (state) => state.app.channelName;
