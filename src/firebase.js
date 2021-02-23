818129d639629ca27142a13387b2a8a4ef48be0c
Clément François <clement.francois.developpeur@outlook.com>
2021-02-23 13:08:09 +0100

    Update firebase.js
    Import firebase, firestore (db), auth and provider to handle google registration

--- src/firebase.js
+++ src/firebase.js
@@ -11,8 +11,9 @@ const firebaseConfig = {
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-const auth = firebase.auth();
 const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
 
-export { db, auth };
+export { db, auth, provider };
 export default db;
