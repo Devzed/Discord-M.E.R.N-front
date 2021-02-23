fb6e5f7e7828aedbb3ade2a6780a813c9514514e
Clément François <clement.francois.developpeur@outlook.com>
2021-02-23 19:06:41 +0100

    Update App.js
    Using the user from the redux part of the app, we can retrieve the user using the useSelector hook and pass the selectUser function to it in order to retrieve the user from the userSlice

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './App.css';
 import Chat from './components/Chat/Chat';
 import Sidebar from './components/Sidebar/Sidebar';
+import { selectUser } from './features/userSlice';
 
 function App() {
+  const user = useSelector(selectUser);
+
+
   return (
     // BEM naming convention
     <div className="app">
-      <Sidebar />
-      <Chat />
+      {user ? (
+        <>
+          <Sidebar />
+          <Chat />
+        </>
+      ) : (
+        <>
+          <Sidebar />
+          <h1 className='app__loginTitle'>You need to login</h1>
+        </>
+      )}
     </div>
   );
 }
